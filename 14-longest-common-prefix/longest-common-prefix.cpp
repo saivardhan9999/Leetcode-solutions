e3f4a5199d33e17d8b156cefdc8f53cf8253d635
Sai Vardhan <vardhans9999@gmail.com>
2026-06-01 12:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (60.02%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return {};
+        string res = strs[0];
+        for(int i=1;i<strs.size();i++)
+        {
+            while(strs[i].find(res)!=0)
+            {
+                res.pop_back();
+                if(res.empty()) return {};
+            }
+        }
+        return res;
+    }
+};
