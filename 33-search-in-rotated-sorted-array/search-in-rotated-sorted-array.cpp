04c5cd878a1e79416e2ad2c3d1ebf345797f90b0
Sai Vardhan <vardhans9999@gmail.com>
2026-06-04 10:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.70%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target && target<nums[mid]) r=mid-1;
+                else l=mid+1;
+            }
+            else
+            {
+                if(target>nums[mid] && target<=nums[r]) l=mid+1;
+                else r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
